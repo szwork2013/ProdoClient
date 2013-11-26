b6c74645716398b3019b2703807a9edf95ef5779
giant leap <bhagyashri@giantleapsystems.com>
2013-11-26 11:00:49 +0530

    feature:comment- Animations added, reply, tags etc done

---
+++ src/app/product/js/prodoCommentEnterKeyDirective.js
@@ -0,0 +1,27 @@
+/*
+ * Overview: ngEnter Directive
+ * It triggers event on enter key press
+ * Dated: 15/11/2013.
+ * Author: Bhagyashri Jangam
+ * Copyright: Prodonus Software Private Limited and GiantLeap Systems Private Limited 2013
+ * Change History:
+ * ----------------------------------------------------------------------
+ * date | author | description 
+ * ----------------------------------------------------------------------
+ * 27-3/2013 | xyx | Add a new property
+ * 
+ */
+angular.module('prodo.ProdoCommentApp')
+        .directive('ngEnter', function() {
+            return function(scope, element, attrs) {
+                element.bind("keydown keypress", function(event) {
+                    if (event.which === 13) {
+                        scope.$apply(function() {
+                            scope.$eval(attrs.ngEnter);
+                        });
+
+                        event.preventDefault();
+                    }
+                });
+            };
+        })
--- src/app/product/js/prodoCommentScrollDirective.js
+++ src/app/product/js/prodoCommentScrollDirective.js
@@ -1,27 +1,28 @@
 /*
-* Overview: commentScroll Directive
-* It will load previous comments when we scroll 
-* Dated: 28/10/2013.
-* Author: Bhagyashri Jangam
-* Copyright: Prodonus Software Private Limited and GiantLeap Systems Private Limited 2013
-* Change History:
-* ----------------------------------------------------------------------
-* date | author | description 
-* ----------------------------------------------------------------------
-* 27-3/2013 | xyx | Add a new property
-* 
-*/
+ * Overview: commentScroll Directive
+ * It will load previous comments when we scroll 
+ * Dated: 28/10/2013.
+ * Author: Bhagyashri Jangam
+ * Copyright: Prodonus Software Private Limited and GiantLeap Systems Private Limited 2013
+ * Change History:
+ * ----------------------------------------------------------------------
+ * date | author | description 
+ * ----------------------------------------------------------------------
+ * 27-3/2013 | xyx | Add a new property
+ * 
+ */
 angular.module('prodo.ProdoCommentApp')
- .directive('whenScrolled', function() {
- 	 return function(scope, elm, attr) {
-        var raw = elm[0];
-        
-        elm.bind('scroll', function() {
-            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
-                scope.$apply(attr.whenScrolled);
-            }
-        });
-    };
-})
+        .directive('whenScrolled', function() {
+            return function(scope, elm, attr) {
+                var raw = elm[0];
+
+                elm.bind('scroll', function() {
+                    if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                        scope.$apply(attr.whenScrolled);
+                    }
+                });
+            };
+        })
+      
 
 
