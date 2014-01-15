f4df96d7ac69e6d619905295cc8ede01a773e3fc
Bhagyashri <bhagyashri@giantleapsystems.com>
2014-01-15 12:51:50 +0530

    Drag Controller

--- src/app/product/js/ProductController.js
+++ src/app/product/js/ProductController.js
@@ -328,6 +328,90 @@ angular.module('prodo.ProductApp')
               }
             }
 
+
+
           }])
+angular.module('prodo.ProductApp')
+        .controller('DragImageController', ['$scope', '$rootScope', 'ProductService', 'UserSessionService', function($scope, $rootScope, ProductService, UserSessionService) {
+
+
+//drag comment image
+function fx()
+{
+document.getElementById("holder").setAttribute('class', 'holderx');
+}
+
+ 
+
+
+var holder = document.getElementById('holder'),
+tests = {
+filereader: typeof FileReader != 'undefined',
+dnd: 'draggable' in document.createElement('span'),
+formdata: !!window.FormData,
+}, 
+support = {
+filereader: document.getElementById('filereader'),
+formdata: document.getElementById('formdata'),
+progress: document.getElementById('progress')
+},
+acceptedTypes = {
+'image/png': true,
+'image/jpeg': true,
+'image/gif': true
+},
+
+fileupload = document.getElementById('upload');
+
+ 
+
+function previewfile(file) {
+if (tests.filereader === true && acceptedTypes[file.type] === true) {
+var reader = new FileReader();
+reader.onload = function (event) {
+var image = new Image();
+image.src = event.target.result; 
+image.width = 250; // a fake resize
+holder.appendChild(image);
+};
+
+reader.readAsDataURL(file);
+} 
+else {
+holder.innerHTML += '<p>Uploaded ' + file.name + ' ' + (file.size ? (file.size/1024|0) + 'K' : '');
+
+}
+}
+
+function readfiles(files) {
+
+var formData = tests.formdata ? new FormData() : null;
+for (var i = 0; i < files.length; i++) 
+{
+if (tests.formdata) formData.append('file', files[i]);
+previewfile(files[i]);
+console.log(files[i].name);
+}
+
+
+}
+
+if (tests.dnd) { 
+holder.ondragover = function () { this.className = 'hover'; return false; };
+holder.ondragend = function () { this.className = ''; return false; };
+holder.ondrop = function (e) 
+{
+this.className = '';
+e.preventDefault();
+readfiles(e.dataTransfer.files);
+}
+
+}
+
+//drag comment image
+
+
+
+}])
 
         
