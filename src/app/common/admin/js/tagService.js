5820a7d4dd4b980d1bce9f5d5243caef7afff309
omkar <omkar@giantleapsystems.com>
2014-04-08 16:51:08 +0530

    fixed admin tags input

--- src/app/common/admin/js/tagService.js
+++ src/app/common/admin/js/tagService.js
@@ -18,3 +18,24 @@ angular.module('prodo.AdminApp').factory('tagAddService', [
     return tagAddObject;
   }
 ]);
+
+angular.module('prodo.AdminApp').factory('domainTagList', [
+  '$rootScope',
+  '$resource',
+  '$state',
+  '$log',
+  function ($rootScope, $resource, $state,$log) {
+    var getAllList = { Product: $resource('/api/tagreffdictionary/alldomaintags', {}, { getTag: { method: 'GET' } }) };
+    var getTagsList = {};
+    getTagsList.getTags = function (tagInputData) {
+      getAllList.Product.getTag(tagInputData, function (success) {
+        $log.debug(success);
+        $rootScope.$broadcast('gotAllDomainTags', success);
+      }), function (error) {
+        $log.debug(error);
+        $rootScope.$broadcast('notGotAllDomainTags', error);
+      };
+    };
+    return getTagsList;
+  }
+]);
