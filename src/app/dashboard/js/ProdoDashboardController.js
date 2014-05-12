bf9c5c08d0b54f3efa8d375d853ed5e0c0ecce3b
omkar <omkar@giantleapsystems.com>
2014-05-12 18:27:35 +0530

    fixed manage campaign banner update

--- src/app/dashboard/js/ProdoDashboardController.js
+++ src/app/dashboard/js/ProdoDashboardController.js
@@ -14,6 +14,10 @@ angular.module('prodo.ProdoWallApp').controller('ProdoDashboardController', [
 
     $scope.showTrendingChart = 0;
 
+    $scope.commentTrending = [];
+
+    $scope.commentObjectStructure = {x:'',y:''};
+
     $scope.barChart = function() {
           return [{
               key: 'Product Ratings',
@@ -58,45 +62,58 @@ angular.module('prodo.ProdoWallApp').controller('ProdoDashboardController', [
     }
 
 
-    // if(trendingChartContent !== undefined)
-    // {
-    //                   if(trendingChartContent.error!==undefined && trendingChartContent.error.code==='AL001')
-    //                   {
-    //                       $rootScope.showModal();
-    //                   }
-    //                   else if(trendingChartContent.error)
-    //                   {
-    //                        $rootScope.ProdoAppMessage(trendingChartContent.error.message,'error');
-    //                        //$rootScope.ProdoAppMessage('Dashboard chart data not available','error');
-    //                   }
-    //                   else
-    //                   {
-                         
-    //                       if(trendingChartContent.success.commenttrending.length>0)
-    //                       {
-    //                        var date = '';
-    //                        for(var i=0;i<trendingChartContent.success.commenttrending.length;i++)
-    //                        {
-    //                         var day = trendingChartContent.success.commenttrending[i].date.day;
-    //                         var month = trendingChartContent.success.commenttrending[i].date.month;
-    //                         var year = trendingChartContent.success.commenttrending[i].date.year;
-    //                         var date = year + '-' + month +'-' + day;
-    //                         console.log("date is "+date);
-    //                        }
-    //                       } 
-    //                   //   $scope.trending = [{"key" : "Talking", value:trendingChartContent.success.producttrending},{"key": "Comments", values : []}];
-    //                   var test = (trendingChartContent.success.producttrending[0].x).split("T");
-    //                   console.log(test[0]);
-    //                   }
+    if(trendingChartContent !== undefined)
+    {
+                      if(trendingChartContent.error!==undefined && trendingChartContent.error.code==='AL001')
+                      {
+                          $rootScope.showModal();
+                      }
+                      else if(trendingChartContent.error)
+                      {
+                           $rootScope.ProdoAppMessage(trendingChartContent.error.message,'error');
+                           //$rootScope.ProdoAppMessage('Dashboard chart data not available','error');
+                      }
+                      else
+                      {
+                         console.log(JSON.stringify(trendingChartContent));
+                          if(trendingChartContent.success.commenttrending.length>0)
+                          {
+                           var date = '';
+                           for(var i=0;i<trendingChartContent.success.commenttrending.length;i++)
+                           {
+                            var day = trendingChartContent.success.commenttrending[i].date.day;
+                            var month = trendingChartContent.success.commenttrending[i].date.month;
+                            var year = trendingChartContent.success.commenttrending[i].date.year;
+                            var date = year + '-' + month +'-' + day;
+                            $scope.commentObjectStructure.x = Date.parse(date);
+                            $scope.commentObjectStructure.y = trendingChartContent.success.commenttrending[i].count;
+                            $scope.commentTrending.push($scope.commentObjectStructure);
+                            console.log("date is "+date);
+                            console.log("----- "+JSON.stringify($scope.commentTrending) );
+                           }
+                          } 
+                         // console.log("Trending "+JSON.stringify(trendingChartContent.success.producttrending));
+                         // console.log("Comments "+JSON.stringify($scope.commentTrending));
+  
+
+                         //$scope.trending = [{"key" : "Talking", values:trendingChartContent.success.producttrending},{"key": "Comments", values : $scope.commentTrending}];
+                      $scope.trending = function()
+                      { 
+                        return [{"key" : "Talking", values:trendingChartContent.success.producttrending},{"key": "Comments", values : $scope.commentTrending}];
+                      };
+                      console.log("Final objext ----" + JSON.stringify($scope.trending()))
+                     // var test = (trendingChartContent.success.producttrending[0].x).split("T");
+                      // console.log(test[0]);
+                      }
 
 
 
-    // }
+    }
 
-    // if(trendingChartContent === undefined)
-    // {
-    //     $rootScope.ProdoAppMessage("There is some issue with the server! Please try again after some time",'error');
-    // }
+    if(trendingChartContent === undefined)
+    {
+        $rootScope.ProdoAppMessage("There is some issue with the server! Please try again after some time",'error');
+    }
 
 
 
